46e7b1a46cd9605df706fb9fd53f94fc9fba0a31
dwajsman <danielwajsman@mac.com>
2021-10-07 04:19:40 -0700

    added for loop to replace numbers with strings

--- loopy.js
+++ loopy.js
@@ -7,10 +7,16 @@
 // If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
 // By print, we are referring to console.log.
 
+for (i = 100; i <= 200; i++) {
 
-
-for (i=100; i<=200; i++){
-
-  console.log(i);
+  if ((x % 3 == 0) && (x % 4 == 0)) {
+    console.log("LoopyLighthouse");
+  } else if (x % 3 == 0) {
+    console.log("Loopy");
+  } else if (x % 4 == 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
 
 }
