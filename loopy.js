32b76b36d04c4b150f2f0cf5cd182aa9a41d5c5a
dwajsman <danielwajsman@mac.com>
2021-10-07 04:13:52 -0700

    Counter from 100 to 200 works

---
+++ loopy.js
@@ -0,0 +1,16 @@
+// CHALLENGE 
+
+// We will be writing a program that prints the numbers from 100 to 200 to the console, with three exceptions:
+
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+// By print, we are referring to console.log.
+
+
+
+for (i=100; i<=200; i++){
+
+  console.log(i);
+
+}
